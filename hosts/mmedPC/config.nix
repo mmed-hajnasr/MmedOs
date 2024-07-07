7afc917e2b06d7150e30f341ecc143d75e79b61b
mmed <mmed.benhadjnasr@gmail.com>
2024-07-07 15:59:44 +0100

    start

---
+++ hosts/mmedPC/config.nix
@@ -0,0 +1,343 @@
+{ config
+, pkgs
+, host
+, username
+, options
+, inputs
+, ...
+}:
+
+{
+  imports = [
+    ./hardware.nix
+    ./users.nix
+    ../../hardware_modules/amd-drivers.nix
+    ../../hardware_modules/nvidia-drivers.nix
+    ../../hardware_modules/nvidia-prime-drivers.nix
+    ../../hardware_modules/intel-drivers.nix
+    ../../hardware_modules/vm-guest-services.nix
+    ../../hardware_modules/local-hardware-clock.nix
+    inputs.xremap-flake.nixosModules.default
+  ];
+
+  boot = {
+    # This is for OBS Virtual Cam Support
+    kernelModules = [ "v4l2loopback" ];
+    extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+    # Needed For Some Steam Games
+    kernel.sysctl = {
+      "vm.max_map_count" = 2147483642;
+    };
+    # Bootloader.
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+
+    # Make /tmp a tmpfs
+    tmp = {
+      useTmpfs = false;
+      tmpfsSize = "30%";
+    };
+
+    # Appimage Support
+    binfmt.registrations.appimage = {
+      wrapInterpreterInShell = false;
+      interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+      recognitionType = "magic";
+      offset = 0;
+      mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+      magicOrExtension = ''\x7fELF....AI\x02'';
+    };
+    plymouth.enable = true;
+  };
+
+  # Extra Module Options
+  drivers.amdgpu.enable = false;
+  drivers.nvidia.enable = true;
+  hardware.nvidia.prime = {
+    offload = {
+      enable = true;
+      enableOffloadCmd = true;
+    };
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
+  drivers.intel.enable = true;
+  vm.guest-services.enable = false;
+  local.hardware-clock.enable = true;
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  networking.hostName = host;
+  networking.timeServers = options.networking.timeServers.default ++ [ "pool.ntp.org" ];
+
+  # Set your time zone.
+  time.timeZone = "Africa/Tunis";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  programs = {
+    zsh.enable = true;
+    virt-manager.enable = true;
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+    };
+    hyprland = {
+      enable = true;
+      xwayland = {
+        enable = true;
+      };
+      portalPackage = pkgs.xdg-desktop-portal-hyprland;
+    };
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  users = {
+    mutableUsers = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    #* dotfiles
+    home-manager
+
+    #* notification
+    inotify-tools
+
+    #* Development
+    git
+    vim
+    gh
+    kitty
+    unzip
+
+    #* sound and video
+    pamixer
+    brightnessctl
+    playerctl
+
+    #* browser
+    firefox
+
+    #* file manager
+    yazi
+
+    #* bluetooth
+    bluez-tools
+    blueberry
+    
+    greetd.tuigreet
+    neovide
+    tree
+    swaynotificationcenter
+  ];
+
+
+  fonts = {
+    packages = with pkgs; [
+      (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+      fira-code
+      noto-fonts-emoji
+      noto-fonts-cjk
+      font-awesome
+      symbola
+      material-icons
+    ];
+  };
+
+  # Extra Portal Configuration
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal
+    ];
+    configPackages = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal
+    ];
+  };
+
+  # Services to start
+  services = {
+    xremap.withHypr = true;
+    xremap.config.modmap = [
+      {
+        name = "Global";
+        remap = { "CapsLock" = "Esc"; }; # globally remap CapsLock to Esc
+      }
+    ];
+    xserver = {
+      enable = false;
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
+    greetd = {
+      enable = true;
+      vt = 3;
+      settings = {
+        default_session = {
+          user = username;
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland"; # start Hyprland with a TUI login manager
+        };
+      };
+    };
+    # monitors the health of the storage devices.
+    smartd = {
+      enable = false;
+      autodetect = true;
+    };
+    libinput.enable = true;
+    fstrim.enable = true;
+    gvfs.enable = true;
+    openssh.enable = true;
+    flatpak.enable = false;
+    printing = {
+      enable = true;
+      drivers = [
+        # pkgs.hplipWithPlugin 
+      ];
+    };
+    gnome.gnome-keyring.enable = true;
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+    ipp-usb.enable = true;
+    syncthing = {
+      enable = true;
+      user = "${username}";
+      dataDir = "/home/${username}";
+      configDir = "/home/${username}/.config/syncthing";
+    };
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+    rpcbind.enable = true;
+    nfs.server.enable = true;
+  };
+  services.dbus = {
+    enable = true;
+    packages = [ pkgs.dconf ];
+  };
+
+  programs.dconf = {
+    enable = true;
+  };
+
+  # scanner support
+  hardware.sane = {
+    enable = true;
+    extraBackends = [ pkgs.sane-airscan ];
+    disabledDefaultBackends = [ "escl" ];
+  };
+
+  # Extra Logitech Support
+  hardware.logitech.wireless.enable = false;
+  hardware.logitech.wireless.enableGraphical = false;
+
+  # Bluetooth Support
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+
+  # Security / Polkit
+  security.rtkit.enable = true;
+  security.polkit.enable = true;
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (
+        subject.isInGroup("users")
+          && (
+            action.id == "org.freedesktop.login1.reboot" ||
+            action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+            action.id == "org.freedesktop.login1.power-off" ||
+            action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+          )
+        )
+      {
+        return polkit.Result.YES;
+      }
+    })
+  '';
+  security.pam.services.swaylock = {
+    text = ''
+      auth include login
+    '';
+  };
+
+  # Optimization settings and garbage collection automation
+  nix = {
+    optimise.automatic = true;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
+
+  # Virtualization / Containers
+  virtualisation.libvirtd.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = true;
+  };
+  hardware.nvidia-container-toolkit.enable = true;
+
+  # OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
---
+++ hosts/mmedPC/variables.nix
@@ -0,0 +1,15 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "mmed";
+  gitEmail = "mmed.benhadjnasr@gmail.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = false;
+
+  # Program Options
+  browser = "firefox"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+}
