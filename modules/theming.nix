05483e865200bff9754c466a1f3befa1c4caa70b
mmed <mmed.benhadjnasr@gmail.com>
2024-07-23 15:50:24 +0100

    small fix

--- modules/theming.nix
+++ modules/theming.nix
@@ -7,7 +7,7 @@
     neofetch
     xorg.xrdb
     (import ../scripts/ghosts.nix { inherit pkgs; })
-    (import ../../scripts/squirtle.nix { inherit pkgs; })
+    (import ../scripts/squirtle.nix { inherit pkgs; })
     (import ../scripts/wall-select.nix { inherit pkgs; })
     (import ../scripts/theme-select.nix { inherit pkgs; })
     (import ../scripts/theme-save.nix { inherit pkgs; })
